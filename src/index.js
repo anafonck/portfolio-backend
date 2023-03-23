7fc87ae4c08af81d24df9be742bb60f3210be72b
githubdaAna <kristinnacruzz@gmail.com>
2023-03-23 17:26:14 -0300

    Implementar middleware para limitar requisicoes por cliente
    Desenvolvimento do middleware rateLimit usando o Express Rate Limit para limitar o numero do requisicoes por usuario

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const router = require("./router")
+const limiter = require("./middlewares/rateLimit")
 
 app.use(express.json());
 app.use(router);
+app.use(limiter);
 
 app.listen(process.env.PORT);
