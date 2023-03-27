ad59b34622f71bd8c790518c09db7842db46b814
githubdaAna <kristinnacruzz@gmail.com>
2023-03-27 14:44:25 -0300

    Config CORS
    Ajustando uso de cors na aplicacao

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const router = require("./router")
 const limiter = require("./middlewares/rateLimit")
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 app.use(limiter);
